5691b2b86eae5bd846a0634bfc7b326cb7bf93ee
Angee Carrión <ajco14987@gmail.com>
2026-02-17 19:11:29 +0100

    Comprobar que pasa el test de ficha

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
@@ -14,5 +14,5 @@ public enum Ficha {
 Substring coge un pedazo de string, aquí le estamos diciendo que coja la primera letra de cada palabra.
 (inicio, fin)= (0, 1) los números son posiciones. Si la palabra es Azul el nombre que devuelve será la A, porque
 se encuentra entre el 0 y el 1.
-
+El mét//odo to String devuelve la primera letra de AZUL y VERDE.
 */

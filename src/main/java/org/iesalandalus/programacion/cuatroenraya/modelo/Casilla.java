93541ab55245efc32a6902d6b7920702f2b59779
Angee Carrión <ajco14987@gmail.com>
2026-02-22 17:11:52 +0100

    Ha pasado la CasillaTest y modificado el toStirng

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -4,7 +4,7 @@ public class Casilla {
     private Ficha ficha; //atributo de la clase
 
     /*Los métodos constructores nos permiten crear objetos e instancia de la clase. En el constructor no se pone el tipo
-        de valor de retorno. El constructor inicializa el objeto*/
+    de valor de retorno. El constructor inicializa el objeto*/
     public Casilla(){ //esto es un constructor vacío que no recibe parámetros.
         this.ficha = null; // le decimos que empieza vacío.
 
@@ -13,7 +13,7 @@ public class Casilla {
     public Ficha getFicha() {
         return ficha;
     }
-//Con esto obtengo los datos y mostrar por pantalla. Muestra u obtiene valores del atributo. Sin modificar nada.
+    //Con esto obtengo los datos y mostrar por pantalla. Muestra u obtiene valores del atributo. Sin modificar nada.
     public void setFicha(Ficha ficha) throws CuatroEnRayaExcepcion{
         if (ficha == null){
             throw new NullPointerException ("No se puede poner una ficha nula.");
@@ -27,26 +27,14 @@ public class Casilla {
     // Al modificar datos hay que validar.
 
     //No necesitamos parámetros porque verifica el estado interno del objeto.
-    public boolean estaOcupada() {
-        if (this.ficha != null){
-            return true;
-        }
-        else {
-            return false;
-        }
+    public boolean estaOcupada(){
+      return this.ficha != null;
     }
+    //me devuelve un true si está ocupada y un false cuando no
 
     @Override
     public String toString() {
-        if(this.ficha == null){
-            return " ";
-        }
-        //Comprobamos si la ficha está en null y devolvemos un espacio en blanco. Aquí se puede con chartAt o substring.
-        String inicial = this.ficha.toString().substring(0,1);
-        //Sacamos la inicial.
-
-        return String.format("%S" , inicial);
-        //devolvemos la inicial en mayúsculas.
+        return String.format("%s", ficha == null ? " " : ficha.toString().substring(0,1));
     }
 
 }
